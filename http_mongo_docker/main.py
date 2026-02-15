24bda629f1e92918190fb92b8739ef8e2c17d3db
ISAC <wastal004@gmail.com>
2026-02-15 12:05:53 +0300

    second commit

--- http_mongo_docker/main.py
+++ http_mongo_docker/main.py
@@ -21,6 +21,6 @@ def get_user(name: str):
     #http://127.0.0.1:8000/user/Wasan
 
 if __name__ == "__main__":
-     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+     uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
